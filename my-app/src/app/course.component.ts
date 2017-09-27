5b8b716407a5c2b2e2ea9b125967ab4df9c46f41
anubha.mishra <anubha.mishra@digite.com>
2017-09-27 21:40:19 +0530

    Event Binding

--- my-app/src/app/course.component.ts
+++ my-app/src/app/course.component.ts
@@ -6,5 +6,13 @@ import {Component} from '@angular/core';
     styleUrls: ['./course.component.css']
 })
 export class CourseComponent {
-    name = 'Angular';
+    name:string = 'Angular';
+    topics:string[]=["Getting Started","Components","Directives","Routing"];
+    addTopic(topic):void{
+        //console.log("Getting called"+topic);
+        this.topics.push(topic);
+    };
+    clearTopic():void{
+        this.topics.splice(0,this.topics.length);
+    }
 }
